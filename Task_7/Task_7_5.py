a8c0daec3f532d52b81ac651698c989b297ff422
Eugene M <56484946+juniours@users.noreply.github.com>
2022-12-21 16:30:20 +0300

    Update Task_7_5.py

--- Task_7/Task_7_5.py
+++ Task_7/Task_7_5.py
@@ -1,10 +1,17 @@
+""" Установить через pip библиотеку numpy. С помощью этой библиотеки cоздать массив 3x3 со случайными значениями, вывести его. Транспонировать его и вывести. """
+
 import numpy as np
 
 a = np.random.randint(0, 10, (3, 3))
 x = '\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in a])
+
+# Вывод матрицы
 print("Исходная матрица 3x3:")
 print(x)
+
 a = a.transpose()
 x = '\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in a])
+
+# Вывод транспонированной матрицы
 print("Транспонированная матрица:")
 print(x)
