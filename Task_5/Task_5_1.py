5d30670aecbe90ee68316e59a391e4c2623bea68
Eugene M <56484946+juniours@users.noreply.github.com>
2022-12-21 16:25:50 +0300

    Update Task_5_1.py

--- Task_5/Task_5_1.py
+++ Task_5/Task_5_1.py
@@ -11,9 +11,9 @@ def check_passwd(p):
     Принимает:
         p - строка с паролем
     """
-    if len(p) >= 6 and not p.isdigit() and not p.isalpha() and not p.isspace()\
-        and p.lower().find('password') == -1
-        and not (' ' in p) and any(n.isdigit() for n in p):
+    if (len(p) >= 6) and (not p.isdigit()) and (not p.isalpha()) and (not p.isspace()) \
+        and (p.lower().find('password') == -1)
+        and (not (' ' in p)) and (any(n.isdigit() for n in p)):
             return True
     else:
         return False
