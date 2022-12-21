2228b4500dbc442fee26cd6bf166060f31aa9328
Eugene M <56484946+juniours@users.noreply.github.com>
2022-12-21 16:23:06 +0300

    Update Task_6_2.py

--- Task_6/Task_6_2.py
+++ Task_6/Task_6_2.py
@@ -1,3 +1,7 @@
+""" Формула молекулы спирта - C2H5(OH). Из неё видно, что молекула состоит из двух атомов углерода (С), 6 атомов водорода (Н) и одного атома кислорода (О). 
+    В input.txt содержится 3 натуральных числа: C, H, O – количество атомов углерода, водорода и кислорода соответственно. 
+    В файл output.txt вывести максимально возможное число молекул спирта, которые могут получиться из атомов, представленных во входных данных. """
+
 with open("input.txt") as f:
     c, h, o = f.read().split()
     try:
@@ -12,11 +16,14 @@ with open("input.txt") as f:
         max = int(float(o))
     except ValueError:
         print("Количество O должно быть числом")
-    if not max and not c and not h:
-        if max > c: max = c
-        if max > h: max = h
+    if (not max) and (not c) and (not h):
+        if max > c:
+            max = c
+        if max > h:
+            max = h
 
 if not max:
     with open("output.txt", "wt") as f:
         f.write("Количество молекул спирта - "+str(max))
-else: print("Введите значения и ")
+else:
+    print("Введите значения и ")
